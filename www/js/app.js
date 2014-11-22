369cf74d3da2a080a6cb7ffc3160229cad88d92c
Hotaviano Melo <melo.hotaviano@gmail.com>
2014-11-22 11:26:29 -0300

    start app

--- www/js/app.js
+++ www/js/app.js
@@ -73,6 +73,6 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       }
     });
   // if none of the above states are matched, use this as the fallback
-  //$urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/start');
 });
 
